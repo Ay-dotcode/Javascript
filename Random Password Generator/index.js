3a72844067db25aac53acd5742d417d3268122d8
Ay-dotcode <joseyowolabi@gmail.com>
2024-06-25 00:12:32 +0300

    joinging html and javascript

--- Random Password Generator/index.js
+++ Random Password Generator/index.js
@@ -1,12 +1,12 @@
-function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
+function generatePassword() {
+    const passwordLen = document.getElementById("noOfChar").value;
+    const includeLowercase = document.getElementById("includeLowercase").checked;
+    const includeUppercase = document.getElementById("includeUppercase").checked;
+    const includeNumbers = document.getElementById("includeNumbers").checked;
+    const includeSymbols = document.getElementById("includeSymbols").checked;
 
-    return '';
+
+    return ``;
 }
 
-
-const passwordLen = 12;
-const includeLowercase = true;
-const includeUppercase = true;
-const includeNumbers = true;
-const includeSymbols = true;
 

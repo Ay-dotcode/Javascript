c7b68c4fbda6065bb840371412783f4dbd52f075
Ay-dotcode <joseyowolabi@gmail.com>
2024-06-24 23:38:42 +0300

    Random Password Generator html/css

---
+++ Random Password Generator/index.js
@@ -0,0 +1,12 @@
+function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
+
+    return '';
+}
+
+
+const passwordLen = 12;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+

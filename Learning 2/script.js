4aa61bbdcf35858d664b17af84252b6a5d50a309
Ay-dotcode <joseyowolabi@gmail.com>
2024-07-03 07:43:40 +0300

    ready for new learning

--- Learning 2/script.js
+++ Learning 2/script.js
@@ -1,50 +1,46 @@
 // var = Global decleration
 // let = Block decleration
 
-class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-    set name(name) {
-        this._name = name;
-    }
-    get name() {
-        return this._name;
-    }
-    set age(age) {
-        this._age = age;
-    }
-    get age() {
-        return this._age;
-    }
-    greet() {
-        return `Hello, My name is ${this.name} and I am ${this.age} years old.`
-    }
-}
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     set name(name) {
+//         this._name = name;
+//     }
+//     get name() {
+//         return this._name;
+//     }
+//     set age(age) {
+//         this._age = age;
+//     }
+//     get age() {
+//         return this._age;
+//     }
+//     greet() {
+//         return `Hello, My name is ${this.name} and I am ${this.age} years old.`
+//     }
+// }
+// class Student extends Person {
+//     constructor(name, age, cgpa) {
+//         super(name, age);
+//         this.cgpa = cgpa;
+//     }
+//     set cgpa(cgpa) {
+//         this._cgpa = cgpa;
+//     }
+//     get cgpa() {
+//         return this._cgpa;
+//     }
+//     greet() {
+//         return `Hello, My name is ${this.name} and I am ${this.age} years old. My CGPA is ${this.cgpa}`
+//     }
+// }
+// let person1 = new Person("Emma", 20);
+// let person2 = new Student("Ay", 18, 3.99);
+// console.log(person1);
+// console.log(person2);
+// console.log(person1.greet());
+// console.log(person2.greet());
 
-class Student extends Person {
-    constructor(name, age, cgpa) {
-        super(name, age);
-        this.cgpa = cgpa;
-    }
-    set cgpa(cgpa) {
-        this._cgpa = cgpa;
-    }
-    get cgpa() {
-        return this._cgpa;
-    }
-    greet() {
-        return `Hello, My name is ${this.name} and I am ${this.age} years old. My CGPA is ${this.cgpa}`
-    }
-}
-
-
-let person1 = new Person("Emma", 20);
-let person2 = new Student("Ay", 18, 3.99);
-
-console.log(person1);
-console.log(person2);
-
-console.log(person1.greet());
-console.log(person2.greet());
